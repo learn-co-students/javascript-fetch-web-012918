c323778569a3c6c394927dbaaece91070a3f6d31
allinthemind66 <r.nilon92@gmail.com>
2018-03-16 16:43:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const token = '0e5ce23189998ed9107de8331083506e66823c1c'
+
 const app = "I don't do much.";
+fetch('https://api.github.com/repos/jquery/jquery/commits',{
+  headers: {
+    Authorization: `token ${token}`
+  }
+
+}).then(res => res.json())
+  .then(json => console.log(json));
