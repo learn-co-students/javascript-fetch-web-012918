5801b6f731c0d85dc9bee386e84b5b8fc298caa9
gilsunshine <gilsunshine@gmail.com>
2018-03-17 13:03:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'd2ee1e2f901e8a3d57daddae5bad7b256a499659'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
