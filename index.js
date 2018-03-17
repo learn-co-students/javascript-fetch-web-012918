df58a0f19358b8539289d9fb34796a622dfdafe4
github username <github email address>
2018-03-17 15:55:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const app = "I don't do much.";
+
+//fetch is a promise. a then function is called after the promise is fulfilled.
+// still used to get information. you can add auth token into fetch
+// fetch can work with multiple data types.
