c3b3dbfe5ebcb82635db6362be9d1fbe235ac243
Camille <cj@mail.co.de>
2018-03-26 23:02:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ fetch('https://api.github.com/repos/jquery/jquery/commits')
   .then(res => res.json())
   .then(json => console.log(json));
 
-const token = 'c963901b6e5d1d802a7d76eaaa70194d1027ac44'
+const token = ''
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
